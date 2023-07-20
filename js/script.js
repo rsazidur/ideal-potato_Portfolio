da72b4e42db97c2d9c1b2e123b7af11cb3f17060
rsazidur <rsazidur936@gmail.com>
2023-07-21 05:52:34 +0600

    Completed

--- js/script.js
+++ js/script.js
@@ -24,6 +24,12 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            // active sections for animation on scroll
+            sec.classList.add('show-animate');
+        }
+        // if want to use animation that repeats on scroll use this
+        else {
+            sec.classList.remove('show-animate');
         }
     });
     let header = document.querySelector('header');
@@ -33,4 +39,10 @@ window.onscroll = () => {
     // remove toggle icon and navbar when click navar links (scroll)
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
+
+    // animation footer on scroll
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+
 }
